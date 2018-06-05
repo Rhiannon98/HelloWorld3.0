f6a81603585e75d65aee9f4be8e652b47d605c74
Rhiannon Mortensen <mortensenrhi@gmail.com>
2018-06-04 21:52:34 -0700

    Add project files.

---
+++ HelloWorld3.0/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld3._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
